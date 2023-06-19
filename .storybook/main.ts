94164b345545fa1f8fd1d7f9232ffba482bd001e
assafchamoy <assafchamoy97@gmail.com>
2023-06-19 21:08:39 +0300

    fix: remove unneeded deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config = {
     addons: [
-      {
-        name: "@storybook/preset-scss",
-        options: {
-          cssLoaderOptions: {
-            importLoaders: 1,
-            modules: {
-              mode: "local",
-              auto: true,
-              localIdentName: "[name]__[local]___[hash:base64:5]",
-              exportGlobals: true,
-            },
-          },
-        },
-      },
-      "@storybook/addon-styling",
       "@storybook/addon-links",
       "@storybook/addon-essentials",
       "@storybook/addon-interactions",
