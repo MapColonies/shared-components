699d309bceaef9909ab348b47927a22fb7af006d
assafchamoy <assafchamoy97@gmail.com>
2023-08-14 12:09:35 +0300

    fix: footprint correction delta

--- packages/react-components/src/components/cesium-map/layers/geojson.layer.tsx
+++ packages/react-components/src/components/cesium-map/layers/geojson.layer.tsx
@@ -6,11 +6,11 @@ import { GeoJsonDataSource as ResiumGeoJsonDataSource } from 'resium';
 export interface RCesiumGeojsonLayerProps extends ComponentProps<typeof ResiumGeoJsonDataSource> {}
 
 const PI_DEGREES = 180;
-const DELTA = 0.0001;
+const DELTA = 0.001;
 const NEGATIVE = -1;
 
 const getSign = (num: number) => Math.abs(num) / num;
-const fixToople = (tuple: Position) => {
+const fixTuple = (tuple: Position) => {
   if (Math.abs(tuple[0]) === PI_DEGREES && Math.abs(tuple[1]) === PI_DEGREES / 2) {
     tuple[0] += NEGATIVE * getSign(tuple[0]) * DELTA; //-180 ==> -180 + 0.0001 = -179.9999;    //180 ==> 180 - 0.0001 = 179.9999;
     tuple[1] += NEGATIVE * getSign(tuple[1]) * DELTA; //-90 ==> -90 + 0.0001 = -89.9999;       //90 ==> 90 - 0.0001 = 89.9999;
@@ -21,13 +21,13 @@ const fixFootprint = (footprint: Geometry): void => {
   switch (footprint.type) {
     case 'LineString':
       (footprint as LineString).coordinates.forEach((tupleArray: Position): void => {
-        fixToople(tupleArray);
+        fixTuple(tupleArray);
       });
       break;
     case 'Polygon':
       (footprint as Polygon).coordinates.forEach((tupleArray: Position[]): void => {
         tupleArray.forEach((tuple: Position): void => {
-          fixToople(tuple);
+          fixTuple(tuple);
         });
       });
       break;
