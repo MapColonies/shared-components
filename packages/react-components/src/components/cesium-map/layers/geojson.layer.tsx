c26466b848b0d2878a1f1030bdb8d99086e52a39
assafchamoy <assafchamoy97@gmail.com>
2023-10-16 22:32:12 +0300

    fix: world footprint normalization

--- packages/react-components/src/components/cesium-map/layers/geojson.layer.tsx
+++ packages/react-components/src/components/cesium-map/layers/geojson.layer.tsx
@@ -1,4 +1,5 @@
 import { Feature, Geometry, LineString, Polygon, Position } from 'geojson';
+import { cloneDeep } from 'lodash';
 import React, { ComponentProps } from 'react';
 
 import { GeoJsonDataSource as ResiumGeoJsonDataSource } from 'resium';
@@ -7,16 +8,18 @@ export interface RCesiumGeojsonLayerProps extends ComponentProps<typeof ResiumGe
 
 const PI_DEGREES = 180;
 const DELTA = 0.001;
-const NEGATIVE = -1;
 
 const getSign = (num: number) => Math.abs(num) / num;
 const fixTuple = (tuple: Position) => {
-  if (Math.abs(tuple[0]) === PI_DEGREES) {
-    tuple[0] += NEGATIVE * getSign(tuple[0]) * DELTA; //-180 ==> -180 + 0.0001 = -179.9999;    //180 ==> 180 - 0.0001 = 179.9999;
+  // (-)180 -> (-)179, (-)90 -> (-)89
+  // In order to avoid dividing by zero when cesium perform all sorts of calculations when drawing.
+  
+  if (PI_DEGREES - Math.abs(tuple[0]) < DELTA) {
+    tuple[0] = getSign(tuple[0]) * (PI_DEGREES - 1);
   }
-
-  if(Math.abs(tuple[1]) === PI_DEGREES / 2) {
-    tuple[1] += NEGATIVE * getSign(tuple[1]) * DELTA; //-90 ==> -90 + 0.0001 = -89.9999;       //90 ==> 90 - 0.0001 = 89.9999;
+  
+  if((PI_DEGREES / 2) - Math.abs(tuple[1]) < DELTA) {
+    tuple[1] = getSign(tuple[1]) * ((PI_DEGREES / 2) - 1);
   }
 };
 
@@ -42,18 +45,18 @@ const fixFootprint = (footprint: Geometry): void => {
 
 export const CesiumGeojsonLayer: React.FC<RCesiumGeojsonLayerProps> = (props) => {
   const { data, ...rest } = props;
-
-  if(data) {
+  const dataCpy = cloneDeep(data);
+  if(dataCpy) {
     // For feature collections
-    if(typeof data.features !== 'undefined') {
-      data.features.forEach((feature: Feature) => {
+    if(typeof dataCpy.features !== 'undefined') {
+      dataCpy.features.forEach((feature: Feature) => {
         fixFootprint(feature.geometry);
       });
     } else {
       // For single geometry
-      fixFootprint(data)
+      fixFootprint(dataCpy);
     }
   }
 
-  return <ResiumGeoJsonDataSource data={data} {...rest} />;
+  return <ResiumGeoJsonDataSource data={dataCpy} {...rest} />;
 };
