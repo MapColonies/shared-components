6c045b155f067e7e4c379c9e1cc59eb7bc48d91e
ellamartirosyan <ellamartirosyan@gmail.com>
2025-05-29 14:02:32 +0300

    chore: align i

--- packages/react-components/src/components/cesium-map/legend/legend-mixin.ts
+++ packages/react-components/src/components/cesium-map/legend/legend-mixin.ts
@@ -25,7 +25,7 @@ export function LegendMixin(viewer: Cesium.Viewer, options: LegendMixinOptions) 
       buttonContainer.className = 'cesium-toolbar-button cesium-button';
 
       const iconSVG = `
-        <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="32" width="26" viewBox="0 0 612 612">
+        <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="32" width="26" viewBox="0 0 612 612" class="center">
           <g xmlns="http://www.w3.org/2000/svg">
             <path d="M322.4,173.9l-129,16.2l-4.6,21.4l25.3,4.7c16.5,3.9,19.8,9.9,16.2,26.4l-41.5,195.3c-10.9,50.5,5.9,74.3,45.5,74.3
               c30.7,0,66.3-14.2,82.5-33.6l4.9-23.4c-11.3,9.9-27.7,13.9-38.6,13.9c-15.5,0-21.1-10.9-17.1-30L322.4,173.9z" />
