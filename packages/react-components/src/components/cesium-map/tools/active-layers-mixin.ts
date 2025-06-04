3e12687674bc70cc6835dac118e0092d22482eb6
ellamartirosyan <ellamartirosyan@gmail.com>
2025-06-04 19:36:37 +0300

    chore: fix active layers css

--- packages/react-components/src/components/cesium-map/tools/active-layers-mixin.ts
+++ packages/react-components/src/components/cesium-map/tools/active-layers-mixin.ts
@@ -25,7 +25,7 @@ export function ActiveLayersMixin(viewer: Cesium.Viewer, options: ActiveLayersMi
 
     createToggleButtonAndDiv() {
       const buttonContainer = document.createElement('div');
-      buttonContainer.className = 'cesium-cesiumInspector-button cesium-button';
+      buttonContainer.className = 'cesium-cesiumInspector-button';
       buttonContainer.innerHTML = get(this.options.locale, 'ACTIVE_LAYERS_TITLE') ?? 'Active Layers';
       buttonContainer.onclick = () => {
         this.toggleContent();
@@ -36,12 +36,12 @@ export function ActiveLayersMixin(viewer: Cesium.Viewer, options: ActiveLayersMi
       this.contentDiv.style.display = 'none';
 
       const inspector = document.createElement('div');
-      inspector.className = 'cesium-cesiumInspector cesium-mcMixin-cesiumInspector cesium-cesiumInspector-visible';
+      inspector.className = 'cesium-cesiumInspector cesium-cesiumInspector-hidden';
       inspector.appendChild(buttonContainer);
       inspector.appendChild(this.contentDiv);
 
       const container = document.createElement('div');
-      container.className = 'cesium-mcMixin-cesiumInspectorContainer';
+      container.className = 'cesium-viewer-cesiumInspectorContainer active-layers';
       container.appendChild(inspector);
 
       const widget = document.querySelector(this.options.containerSelector);
@@ -54,7 +54,7 @@ export function ActiveLayersMixin(viewer: Cesium.Viewer, options: ActiveLayersMi
 
     populateContent() {
       if (this.contentDiv) {
-        this.contentDiv.innerHTML = `bla bla bla\n bla1 bla2\n bla3 bla4`;
+        this.contentDiv.innerHTML = `bla bla bla\n bla1 bla2\n bla3 bla4<br/>kuku muku kuku muku`;
       }
     }
 
