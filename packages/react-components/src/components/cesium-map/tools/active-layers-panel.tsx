14ac2fbb4e6e49b3bf5df6b5e9ce1382d3eb07e4
ellamartirosyan <ellamartirosyan@gmail.com>
2025-06-08 07:01:49 +0300

    chore: accept dynamic sections for greater flexibility and reusability

--- packages/react-components/src/components/cesium-map/tools/active-layers-panel.tsx
+++ packages/react-components/src/components/cesium-map/tools/active-layers-panel.tsx
@@ -7,22 +7,26 @@ import { CesiumViewer, useCesiumMap } from '../map';
 
 import './active-layers-panel.css';
 
+interface ISection {
+  id: string;
+  labelKey: string;
+  content: JSX.Element | string;
+}
+
 interface IActiveLayersPanelProps {
   viewer?: CesiumViewer;
   locale?: { [key: string]: string };
+  sections: ISection[];
 }
 
-export const ActiveLayersPanel: React.FC<IActiveLayersPanelProps> = ({ viewer, locale }) => {
+export const ActiveLayersPanel: React.FC<IActiveLayersPanelProps> = ({ viewer, locale, sections }) => {
   const mapViewer = viewer ?? useCesiumMap();
   const [active, setActive] = useState<ICesiumImageryLayer[]>([]);
-  const [collapsedRaster, setCollapsedRaster] = useState(true);
-  const [collapsed3D, setCollapsed3D] = useState(true);
-  const [collapsedDEM, setCollapsedDEM] = useState(true);
-  const [collapsedVector, setCollapsedVector] = useState(true);
-  const rasterLabel = useMemo(() => get(locale, 'RASTER_SECTION') ?? 'Raster', [locale]);
-  const threeDLabel = useMemo(() => get(locale, '3D_SECTION') ?? '3D', [locale]);
-  const demLabel = useMemo(() => get(locale, 'DEM_SECTION') ?? 'DEM', [locale]);
-  const vectorLabel = useMemo(() => get(locale, 'VECTOR_SECTION') ?? 'Vector', [locale]);
+  const [collapsedSections, setCollapsedSections] = useState<Record<string, boolean>>(
+    sections.reduce((acc, section) => ({ ...acc, [section.id]: true }), {})
+  );
+
+  const getLabel = (key: string) => useMemo(() => get(locale, key) ?? key, [locale]);
 
   useEffect(() => {
     if (!mapViewer.layersManager) return;
@@ -48,7 +52,6 @@ export const ActiveLayersPanel: React.FC<IActiveLayersPanelProps> = ({ viewer, l
       });
 
       const activeLayersIds = new Set(mapViewer.layersManager?.layerList.map((layer) => layer.meta?.id));
-
       setActive((prevActive) => prevActive.filter((item) => activeLayersIds.has(item.meta?.id)));
     };
 
@@ -59,50 +62,28 @@ export const ActiveLayersPanel: React.FC<IActiveLayersPanelProps> = ({ viewer, l
     };
   }, [mapViewer.layersManager?.layerList]);
 
-  const toggleRaster = () => {
-    setCollapsedRaster((prev) => !prev);
-  };
-
-  const toggle3D = () => {
-    setCollapsed3D((prev) => !prev);
-  };
-
-  const toggleDEM = () => {
-    setCollapsedDEM((prev) => !prev);
-  };
-
-  const toggleVector = () => {
-    setCollapsedVector((prev) => !prev);
+  const toggleSection = (id: string) => {
+    setCollapsedSections((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
   return (
     <Box className="activeLayersContainer">
-      <Box className={`cesium-cesiumInspector-section ${collapsedRaster ? 'cesium-cesiumInspector-section-collapsed' : ''}`} onClick={toggleRaster}>
-        <Typography tag="h3" className="cesium-cesiumInspector-sectionHeader">{rasterLabel}</Typography>
-        <Box className="cesium-cesiumInspector-sectionContent">
-          <Box>Bla bla bla</Box>
-          <Box>Kuku</Box>
-          <Box>Muku</Box>
-        </Box>
-      </Box>
-      <Box className={`cesium-cesiumInspector-section ${collapsed3D ? 'cesium-cesiumInspector-section-collapsed' : ''}`} onClick={toggle3D}>
-        <Typography tag="h3" className="cesium-cesiumInspector-sectionHeader">{threeDLabel}</Typography>
-        <Box className="cesium-cesiumInspector-sectionContent">
-          N/A
-        </Box>
-      </Box>
-      <Box className={`cesium-cesiumInspector-section ${collapsedDEM ? 'cesium-cesiumInspector-section-collapsed' : ''}`} onClick={toggleDEM}>
-        <Typography tag="h3" className="cesium-cesiumInspector-sectionHeader">{demLabel}</Typography>
-        <Box className="cesium-cesiumInspector-sectionContent">
-          N/A
-        </Box>
-      </Box>
-      <Box className={`cesium-cesiumInspector-section ${collapsedVector ? 'cesium-cesiumInspector-section-collapsed' : ''}`} onClick={toggleVector}>
-        <Typography tag="h3" className="cesium-cesiumInspector-sectionHeader">{vectorLabel}</Typography>
-        <Box className="cesium-cesiumInspector-sectionContent">
-          N/A
-        </Box>
-      </Box>
+      {
+        sections.map((section) => (
+          <Box
+            key={section.id}
+            className={`cesium-cesiumInspector-section ${collapsedSections[section.id] ? 'cesium-cesiumInspector-section-collapsed' : ''}`}
+            onClick={() => toggleSection(section.id)}
+          >
+            <Typography tag="h3" className="cesium-cesiumInspector-sectionHeader">
+              {getLabel(section.labelKey)}
+            </Typography>
+            <Box className="cesium-cesiumInspector-sectionContent">
+              {section.content}
+            </Box>
+          </Box>
+        ))
+      }
       {
         active.filter((layer) => layer.meta?.type === 'raster').map((layer, index) => (
           <Box key={index} className="layer">
