8851bd4e0485c971b14649aa428d6d011a86fb98
ellamartirosyan <ellamartirosyan@gmail.com>
2025-07-29 20:31:22 +0300

    chore: convert base maps from mixin to widget

--- packages/react-components/src/components/cesium-map/base-map/base-maps-panel.tsx
+++ packages/react-components/src/components/cesium-map/base-map/base-maps-panel.tsx
@@ -18,9 +18,8 @@ interface BaseMapsPanelProps {
   baseMaps?: IBaseMaps;
 }
 
-export const BaseMapsPanel: React.FC<BaseMapsPanelProps> = (props) => {
+export const BaseMapsPanel: React.FC<BaseMapsPanelProps> = ({ baseMaps }) => {
   const mapViewer: CesiumViewer = useCesiumMap();
-  const { baseMaps } = props;
   const [currentMap, setCurrentMap] = useState<string>(' ');
   const [selectedBaseMap, setSelectedBaseMap] = useState<IBaseMap | undefined>();
   const classes = useStyle();
@@ -81,26 +80,34 @@ export const BaseMapsPanel: React.FC<BaseMapsPanelProps> = (props) => {
   );
 };
 
-{/* <div class="cesium-baseLayerPicker-sectionTitle" data-bind="visible: imageryProviderViewModels.length > 0">Base Map</div>
-<div class="cesium-baseLayerPicker-section" data-bind="foreach: _imageryProviders">
+{/* <div class="cesium-baseLayerPicker-sectionTitle">Base Map</div>
+<div class="cesium-baseLayerPicker-section">
   <div class="cesium-baseLayerPicker-category">
-    <div class="cesium-baseLayerPicker-categoryTitle" data-bind="text: name"></div>
-    <div class="cesium-baseLayerPicker-choices" data-bind="foreach: providers">
-      <div class="cesium-baseLayerPicker-item cesium-baseLayerPicker-selectedItem" data-bind="css: { &quot;cesium-baseLayerPicker-selectedItem&quot; : $data === $parents[1].selectedImagery },attr: { title: tooltip },visible: creationCommand.canExecute,click: function($data) { $parents[1].selectedImagery = $data; }" title="1st Map"><img class="cesium-baseLayerPicker-itemIcon" data-bind="attr: { src: iconUrl }" draggable="false" src="assets/img/1st.png"><div class="cesium-baseLayerPicker-itemLabel" data-bind="text: name">1st Map</div></div><div class="cesium-baseLayerPicker-item" data-bind="css: { &quot;cesium-baseLayerPicker-selectedItem&quot; : $data === $parents[1].selectedImagery },attr: { title: tooltip },visible: creationCommand.canExecute,click: function($data) { $parents[1].selectedImagery = $data; }" title="2nd Map"><img class="cesium-baseLayerPicker-itemIcon" data-bind="attr: { src: iconUrl }" draggable="false" src="assets/img/2nd.png"><div class="cesium-baseLayerPicker-itemLabel" data-bind="text: name">2nd Map</div></div><div class="cesium-baseLayerPicker-item" data-bind="css: { &quot;cesium-baseLayerPicker-selectedItem&quot; : $data === $parents[1].selectedImagery },attr: { title: tooltip },visible: creationCommand.canExecute,click: function($data) { $parents[1].selectedImagery = $data; }" title="3rd Map">
-        <img class="cesium-baseLayerPicker-itemIcon" data-bind="attr: { src: iconUrl }" draggable="false" src="assets/img/3rd.png">
-        <div class="cesium-baseLayerPicker-itemLabel" data-bind="text: name">3rd Map</div>
+    <div class="cesium-baseLayerPicker-categoryTitle"></div>
+    <div class="cesium-baseLayerPicker-choices">
+      <div class="cesium-baseLayerPicker-item cesium-baseLayerPicker-selectedItem" title="1st Map">
+        <img class="cesium-baseLayerPicker-itemIcon" src="assets/img/1st.png" />
+        <div class="cesium-baseLayerPicker-itemLabel">1st Map</div>
+      </div>
+      <div class="cesium-baseLayerPicker-item" title="2nd Map">
+        <img class="cesium-baseLayerPicker-itemIcon" src="assets/img/2nd.png" />
+        <div class="cesium-baseLayerPicker-itemLabel">2nd Map</div>
+      </div>
+      <div class="cesium-baseLayerPicker-item" title="3rd Map">
+        <img class="cesium-baseLayerPicker-itemIcon" src="assets/img/3rd.png" />
+        <div class="cesium-baseLayerPicker-itemLabel">3rd Map</div>
       </div>
     </div>
   </div>
 </div>
-<div class="cesium-baseLayerPicker-sectionTitle" data-bind="visible: terrainProviderViewModels.length > 0">Terrain</div>
-<div class="cesium-baseLayerPicker-section" data-bind="foreach: _terrainProviders">
+<div class="cesium-baseLayerPicker-sectionTitle">Terrain</div>
+<div class="cesium-baseLayerPicker-section">
   <div class="cesium-baseLayerPicker-category">
-    <div class="cesium-baseLayerPicker-categoryTitle" data-bind="text: name"></div>
-    <div class="cesium-baseLayerPicker-choices" data-bind="foreach: providers">
-      <div class="cesium-baseLayerPicker-item cesium-baseLayerPicker-selectedItem" data-bind="css: { &quot;cesium-baseLayerPicker-selectedItem&quot; : $data === $parents[1].selectedTerrain },attr: { title: tooltip },visible: creationCommand.canExecute,click: function($data) { $parents[1].selectedTerrain = $data; }" title="Default Terrain">
-        <img class="cesium-baseLayerPicker-itemIcon" data-bind="attr: { src: iconUrl }" draggable="false" src="Cesium/Widgets/Images/TerrainProviders/Ellipsoid.png">
-        <div class="cesium-baseLayerPicker-itemLabel" data-bind="text: name">Terrain</div>
+    <div class="cesium-baseLayerPicker-categoryTitle"></div>
+    <div class="cesium-baseLayerPicker-choices">
+      <div class="cesium-baseLayerPicker-item cesium-baseLayerPicker-selectedItem" title="Default Terrain">
+        <img class="cesium-baseLayerPicker-itemIcon" src="Cesium/Widgets/Images/TerrainProviders/Ellipsoid.png">
+        <div class="cesium-baseLayerPicker-itemLabel">Terrain</div>
       </div>
     </div>
   </div>
