c1c90baa0e30c8d27536e56a49deef269d8de8d8
ellamartirosyan <ellamartirosyan@gmail.com>
2025-08-06 21:50:32 +0300

    chore: tooltip

--- packages/react-components/src/components/cesium-map/active-layers/active-layers-panel.tsx
+++ packages/react-components/src/components/cesium-map/active-layers/active-layers-panel.tsx
@@ -1,7 +1,7 @@
 import { Rectangle } from 'cesium';
 import { get } from 'lodash';
 import React, { useEffect, useState } from 'react';
-import { Typography } from '@map-colonies/react-core';
+import { Tooltip, Typography } from '@map-colonies/react-core';
 import bbox from '@turf/bbox';
 import { Box } from '../../box';
 import { useCesiumMap } from '../map';
@@ -147,21 +147,25 @@ export const ActiveLayersPanel: React.FC<IActiveLayersPanelProps> = ({ locale })
               {
                 section.values.map((activeLayer: IActiveLayer) => (
                   <Box key={activeLayer.id} className="layer">
-                    <Box className="name">{activeLayer.name}</Box>
+                    <Tooltip content={activeLayer.name}>
+                      <Box className={`name ${activeLayer.isBaseMap ? 'disabled' : ''}`}>{activeLayer.name}</Box>
+                    </Tooltip>
                     <Box className="icons">
-                      <Box className="icon" onClick={(event) => { event.stopPropagation(); handleFlyTo(activeLayer.rect); }}>
-                        <svg fill="var(--mdc-theme-cesium-color)" width="100%" height="100%" viewBox="0 0 256 256">
-                          <path d="M236,120H223.66406A96.15352,96.15352,0,0,0,136,32.33618V20a8,8,0,0,0-16,0V32.33618A96.15352,96.15352,0,0,0,32.33594,120H20a8,8,0,0,0,0,16H32.33594A96.15352,96.15352,0,0,0,120,223.66382V236a8,8,0,0,0,16,0V223.66382A96.15352,96.15352,0,0,0,223.66406,136H236a8,8,0,0,0,0-16Zm-40,16h11.59912A80.14164,80.14164,0,0,1,136,207.59912V196a8,8,0,0,0-16,0v11.59912A80.14164,80.14164,0,0,1,48.40088,136H60a8,8,0,0,0,0-16H48.40088A80.14164,80.14164,0,0,1,120,48.40088V60a8,8,0,0,0,16,0V48.40088A80.14164,80.14164,0,0,1,207.59912,120H196a8,8,0,0,0,0,16Zm-28-8a40,40,0,1,1-40-40A40.04552,40.04552,0,0,1,168,128Z"/>
-                        </svg>
-                      </Box>
-                      {
-                        !activeLayer.isBaseMap &&
-                        <Box className="icon" onClick={() => {}}>
-                          <svg width="100%" height="100%" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="var(--mdc-theme-cesium-color)">
+                      <Tooltip content={get(locale, 'FLY_TO') ?? 'Fly To'}>
+                        <Box className="icon" onClick={(event) => { event.stopPropagation(); handleFlyTo(activeLayer.rect); }}>
+                          <svg fill="var(--mdc-theme-cesium-color)" width="100%" height="100%" viewBox="0 0 256 256">
+                            <path d="M236,120H223.66406A96.15352,96.15352,0,0,0,136,32.33618V20a8,8,0,0,0-16,0V32.33618A96.15352,96.15352,0,0,0,32.33594,120H20a8,8,0,0,0,0,16H32.33594A96.15352,96.15352,0,0,0,120,223.66382V236a8,8,0,0,0,16,0V223.66382A96.15352,96.15352,0,0,0,223.66406,136H236a8,8,0,0,0,0-16Zm-40,16h11.59912A80.14164,80.14164,0,0,1,136,207.59912V196a8,8,0,0,0-16,0v11.59912A80.14164,80.14164,0,0,1,48.40088,136H60a8,8,0,0,0,0-16H48.40088A80.14164,80.14164,0,0,1,120,48.40088V60a8,8,0,0,0,16,0V48.40088A80.14164,80.14164,0,0,1,207.59912,120H196a8,8,0,0,0,0,16Zm-28-8a40,40,0,1,1-40-40A40.04552,40.04552,0,0,1,168,128Z"/>
+                            <polygon points="168,128 160,120 160,136" fill="var(--mdc-theme-cesium-color)"/>
+                          </svg>
+                        </Box>
+                      </Tooltip>
+                      <Tooltip content={get(locale, 'REMOVE') ?? 'Remove'}>
+                        <Box className={`icon ${activeLayer.isBaseMap ? 'disabled' : ''}`} onClick={() => {}}>
+                          <svg width="100%" height="100%" viewBox="0 0 16 16" fill="var(--mdc-theme-cesium-color)">
                             <path fillRule="evenodd" clipRule="evenodd" d="M10 3h3v1h-1v9l-1 1H4l-1-1V4H2V3h3V2a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v1zM9 2H6v1h3V2zM4 13h7V4H4v9zm2-8H5v7h1V5zm1 0h1v7H7V5zm2 0h1v7H9V5z"/>
                           </svg>
                         </Box>
-                      }
+                      </Tooltip>
                     </Box>
                   </Box>
                 ))
