d34bfbb312bb452fd7bfd24a7aa8f6d3bbd7e65d
ellamartirosyan <ellamartirosyan@gmail.com>
2025-08-07 08:42:15 +0300

    chore: stop propagation

--- packages/react-components/src/components/cesium-map/active-layers/active-layers-panel.tsx
+++ packages/react-components/src/components/cesium-map/active-layers/active-layers-panel.tsx
@@ -160,7 +160,7 @@ export const ActiveLayersPanel: React.FC<IActiveLayersPanelProps> = ({ locale })
                         </Box>
                       </Tooltip>
                       <Tooltip content={get(locale, 'REMOVE') ?? 'Remove'}>
-                        <Box className={`icon ${activeLayer.isBaseMap ? 'disabled' : ''}`} onClick={() => {}}>
+                        <Box className={`icon ${activeLayer.isBaseMap ? 'disabled' : ''}`} onClick={(event) => { event.stopPropagation(); }}>
                           <svg width="100%" height="100%" viewBox="0 0 16 16" fill="var(--mdc-theme-cesium-color)">
                             <path fillRule="evenodd" clipRule="evenodd" d="M10 3h3v1h-1v9l-1 1H4l-1-1V4H2V3h3V2a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v1zM9 2H6v1h3V2zM4 13h7V4H4v9zm2-8H5v7h1V5zm1 0h1v7H7V5zm2 0h1v7H9V5z"/>
                           </svg>
