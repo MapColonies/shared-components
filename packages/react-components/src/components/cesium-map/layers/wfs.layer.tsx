bc0160d378b6c0d92ca49df22b0df6646411a97d
ellamartirosyan <ellamartirosyan@gmail.com>
2025-09-10 18:16:04 +0300

    chore: fix

--- packages/react-components/src/components/cesium-map/layers/wfs.layer.tsx
+++ packages/react-components/src/components/cesium-map/layers/wfs.layer.tsx
@@ -106,7 +106,7 @@ export const CesiumWFSLayer: React.FC<ICesiumWFSLayer> = (props) => {
         const { fieldName, aliasFieldName } = field;
         const key = aliasFieldName;
         const value = properties[fieldName] ?? 'N/A';
-        const keyMaxWidth = Math.max(100, Math.min(200, key.length * 10));
+        const keyMaxWidth = Math.max(100, Math.min(180, key.length * 10));
         const valueMaxWidth = '260px';
         rows.push(`
           <tr>
