c0a860ce27056d083d408bad5a3f2d7c74b36dbd
ellamartirosyan <ellamartirosyan@gmail.com>
2026-06-29 11:18:17 +0300

    fix: fix

--- packages/react-components/src/components/cesium-map/layers/xyz.layer.stories.tsx
+++ packages/react-components/src/components/cesium-map/layers/xyz.layer.stories.tsx
@@ -1,4 +1,7 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { ImageryLayer } from 'cesium';
+import { BASE_MAPS } from '../helpers/constants';
+import { getImageryProviderUrl } from '../layers-manager';
 import { CesiumMap } from '../map';
 import { CesiumXYZLayer } from './xyz.layer';
 
@@ -31,13 +34,33 @@ const optionsXYZ2 = {
   url: 'https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=0e6fc415256d4fbb9b5166a718591d71',
 };
 
+const xyzLayerMeta = {
+  id: 'xyz-layer',
+  layerRecord: {
+    productName: 'XYZ Layer',
+  },
+  options: { ...optionsXYZ },
+  searchLayerPredicate: (layer: ImageryLayer): boolean => getImageryProviderUrl(layer) === optionsXYZ.url,
+};
+
+const xyzLayerMeta2 = {
+  id: 'xyz-layer-2',
+  layerRecord: {
+    productName: 'XYZ Layer 2',
+  },
+  options: { ...optionsXYZ2 },
+  searchLayerPredicate: (layer: ImageryLayer): boolean => getImageryProviderUrl(layer) === optionsXYZ2.url,
+};
+
 export const MapWithXYZLayers: Story = () => (
   <div style={mapDivStyle}>
     <CesiumMap
+      imageryProvider={false}
+      baseMaps={BASE_MAPS}
       layerManagerMetaMapping={layerManagerMetaMapping}
     >
-      <CesiumXYZLayer options={optionsXYZ} />
-      <CesiumXYZLayer options={optionsXYZ2} alpha={0.5} />
+      <CesiumXYZLayer options={optionsXYZ} meta={xyzLayerMeta} />
+      <CesiumXYZLayer options={optionsXYZ2} alpha={0.5} meta={xyzLayerMeta2} />
     </CesiumMap>
   </div>
 );
